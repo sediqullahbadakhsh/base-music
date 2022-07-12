6bf10c4c73af1cdd266617fd4d88c4c3190238de
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-07-13 02:03:31 +0430

    fix show action

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -5,6 +5,6 @@ class AlbumsController < ApplicationController
 
     def show
         @album = Album.find(params[:id])
-        @tracks = @album.reviews
+        @tracks = @album.tracks
     end
 end
