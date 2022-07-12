c4f497b56efefc163fea91e1c8404d62fde94b39
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-07-13 01:42:08 +0430

    update tracks migration

--- db/migrate/20220712210644_create_tracks.rb
+++ db/migrate/20220712210644_create_tracks.rb
@@ -1,6 +1,9 @@
 class CreateTracks < ActiveRecord::Migration[7.0]
   def change
     create_table :tracks do |t|
+      t.string :name
+      t.string :minutes
+      t.references :album
 
       t.timestamps
     end
